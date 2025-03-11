ced52831819bf836b786b2c1e548b236ef7f0d1e
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-11 00:04:15 -0400

    Crea la primera migración

---
+++ TiendaHD/Migrations/20250311035910_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaHD.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Financiamientos",
+                columns: table => new
+                {
+                    FinanciamientoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Detalle = table.Column<string>(type: "TEXT", nullable: false),
+                    CuotaMensual = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Financiamientos", x => x.FinanciamientoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Financiamientos");
+        }
+    }
+}
