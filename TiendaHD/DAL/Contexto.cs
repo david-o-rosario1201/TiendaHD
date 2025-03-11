7fce9ed924e906ef6cd928d909e8516df0db5631
HenzoWTF <Urenahenzo@gmail.com>
2025-03-11 15:59:42 -0400

    Agrega UI de Gastos Administrativo

--- TiendaHD/DAL/Contexto.cs
+++ TiendaHD/DAL/Contexto.cs
@@ -11,6 +11,6 @@ public class Contexto : DbContext
     }
 
     public DbSet<Financiamientos> Financiamientos { get; set; }
-
+    public DbSet<GastosADM> GastosADM { get; set; }
     public DbSet<Empleados> Empleados { get; set; }
 }
