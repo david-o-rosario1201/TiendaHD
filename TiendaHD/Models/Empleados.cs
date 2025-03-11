1d6ff98b3ba951121c77b13fc7508668f8befc2f
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-11 15:17:24 -0400

    Crea el modelo Empleados

---
+++ TiendaHD/Models/Empleados.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaHD.Models;
+
+public class Empleados
+{
+	[Key]
+	public int EmpleadoId { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string Puesto { get; set; }
+
+    public decimal Salario { get; set; }
+}
