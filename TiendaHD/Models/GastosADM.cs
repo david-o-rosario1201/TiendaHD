0a28c3db1a66c899ffc87a30f7535140e29a84b1
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-11 01:08:27 -0400

    Crea los modelos GastosADM y GastosMenores

---
+++ TiendaHD/Models/GastosADM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaHD.Models;
+
+public class GastosADM
+{
+	[Key]
+	public int GastosADMId { get; set; }
+	public string GastosADMName { get; set; }
+	public float ValorGastoADM { get; set; }
+}
---
+++ TiendaHD/Models/GastosMenores.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaHD.Models;
+
+public class GastosMenores
+{
+	[Key]
+	public int GastosMId { get; set; }
+	public string GastosMName { get; set; }
+	public float ValorGastosM { get; set; }
+}
