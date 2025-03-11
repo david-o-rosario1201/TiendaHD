82f9b887db2035c1c8eea36ee9d1753e566d46d3
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-11 00:02:14 -0400

    Crea el modelo Financiamientos

---
+++ TiendaHD/Models/Financiamientos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaHD.Models;
+
+public class Financiamientos
+{
+    [Key]
+    public int FinanciamientoId { get; set; }
+
+    public string Detalle { get; set; }
+
+    public decimal CuotaMensual { get; set; }
+}
