f42f681e21048d1d04aa1f483861a3b6d3c7fa3e
david-o-rosario1201 <finnnn023@gmail.com>
2025-03-12 09:00:01 -0400

    Crea el modelo Compras

---
+++ TiendaHD/Models/Compras.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaHD.Models;
+
+public class Compras
+{
+    [Key]
+    public int CompraId { get; set; }
+
+    public int CantidadPantalones { get; set; }
+
+    public int CantidadPerfumes { get; set; }
+
+    public int CantidadSweater { get; set; }
+
+	public decimal PrecioPantalones { get; set; }
+
+	public decimal PrecioPerfumes { get; set; }
+
+	public decimal PrecioSweater { get; set; }
+
+    public decimal PagoChofer { get; set; }
+
+    public decimal PagoComidaChofer { get; set; }
+
+    public decimal PagoGasolina { get; set; }
+}
