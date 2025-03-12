8513fa70c619609e9c4806f31d85b93232c045a9
HenzoWTF <Urenahenzo@gmail.com>
2025-03-11 22:34:14 -0400

    Agregar Gastos menores a migracion

---
+++ TiendaHD/Migrations/20250312023329_GastosMenores.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaHD.Migrations
+{
+    /// <inheritdoc />
+    public partial class GastosMenores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GastosMenores",
+                columns: table => new
+                {
+                    GastosMId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GastosMName = table.Column<string>(type: "TEXT", nullable: false),
+                    ValorGastosM = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GastosMenores", x => x.GastosMId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GastosMenores");
+        }
+    }
+}
